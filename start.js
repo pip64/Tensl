fc4e45a3c5ba1e65ad6176e604801642ec4e3b31
pip64 <76626915+pip64@users.noreply.github.com>
2023-06-02 18:17:05 +0700

    While, for, use

--- start.js
+++ start.js
@@ -15,18 +15,27 @@ function translator(str) {
   );
   str = str.replace(/if\.\s*<(.*?)>\s*start/g, 'if ($1) {');
   str = str.replace(/finish/g, '}');
+  str = str.replace(/while\.\s*<(.*?)>\s*start/g, 'while ($1) {');
+  str = str.replace(/finish/g, '}');
+  str = str.replace(/for\.\s*<(.*?)>\s*start/g, 'for ($1) {');
+  str = str.replace(/finish/g, '}');
   str = str.replace(/let\s+box\/(\w+)/g, "let $1");
   str = str.replace(/bad\s+box\/(\w+)/g, "var $1");
   str = str.replace(/con\s+box\/(\w+)/g, "const $1");
   str = str.replace(/box\/(\w+)/g, "$1");
   str = str.replace(/inputLine\.\s*(.*)/g, "readlineSync.question($1)");
   str = str.replace(/(\w+)\.\(([^)]+)\)/g, "$1($2)");
-  str = str.replace(/\.\[(\w+)\]/g, '${$1}');
+  str = str.replace(/\.\[(.+?)\]/g, '${$1}');
   str = str.replace(/(?<!\\)<|(?<!\\)>/g, '`');
   str = str.replace(/\\\\/g, "");
   
   return str;
 }
+
+input = input.replace(/use\.<(.*?)>/g, (match, filename) => {
+  const content = fs.readFileSync(filename, 'utf8');
+  return translator(content);
+});
 
 var output = translator(input);
 
